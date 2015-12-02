4fe5a422eb1c56c2ccc68d3115706c0a267fff81
Harris <skyshow520@vip.qq.com>
2015-12-02 15:25:10 +0800

    Add View
    Add View 添加文件

---
+++ CirclePatterning/CirclePatterning/SNView.h
@@ -0,0 +1,13 @@
+//
+//  SNView.h
+//  CirclePatterning
+//
+//  Created by JianRongCao on 15/12/2.
+//  Copyright © 2015年 Cao JianRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNView : UIView
+
+@end
---
+++ CirclePatterning/CirclePatterning/SNView.m
@@ -0,0 +1,21 @@
+//
+//  SNView.m
+//  CirclePatterning
+//
+//  Created by JianRongCao on 15/12/2.
+//  Copyright © 2015年 Cao JianRong. All rights reserved.
+//
+
+#import "SNView.h"
+
+@implementation SNView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
